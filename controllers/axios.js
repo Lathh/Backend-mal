1524d0cedf8e34f6f62b2463cba46c180b8f1d59
AurelliaChristie <aurelliachristie77@gmail.com>
2021-12-23 16:01:05 +0700

    Add all endpoints from 3rd API

--- controllers/axios.js
+++ controllers/axios.js
@@ -1,14 +1,58 @@
 const axios = require("axios");
 
+// Track
+const topTracks = async (req,res)  => {
+    try {
+        const response = await axios.get('https://deezer.eddypermana.com/chart/0/tracks');
+        res.status(200).json({
+            message: "Top Tracks",
+            data: response.data.data
+        });
+    } catch (error) {
+        res.status(404).json({
+            message: error.message
+        });
+    }
+}
 
+// Genre
+const allGenres = async (req,res)  => {
+    try {
+        const response = await axios.get('https://deezer.eddypermana.com/genre');
+        res.status(200).json({
+            message: "All Genres",
+            data: response.data.data
+        });
+    } catch (error) {
+        res.status(404).json({
+            message: error.message
+        });
+    }
+}
 
-const topAlbum  = async (req, res) => {
+const genreArtists = async (req,res)  => {
+    try {
+        const genreId = req.params.genreId;
+        const response = await axios.get(`https://deezer.eddypermana.com/genre/${genreId}/artists`);
+        res.status(200).json({
+            message: "Genre Artists",
+            data: response.data.data
+        });
+    } catch (error) {
+        res.status(404).json({
+            message: error.message
+        });
+    }
+}
+
+// Album
+const topAlbums  = async (req, res) => {
     try {
         const response = await axios.get('https://deezer.eddypermana.com/chart/0/albums')
         //console.log(response.data)
 
         res.status(200).json({
-            message: "Data Top Albums",
+            message: "Top Albums",
             data: response.data.data
         })
     } catch (error) {
@@ -18,59 +62,199 @@ const topAlbum  = async (req, res) => {
     }
 }
 
-const topGenre = async (req,res)  => {
+const albumById  = async (req, res) => {
     try {
-        const response = await axios.get('https://deezer.eddypermana.com/genre')
+        const albumId = req.params.albumId;
+        const response = await axios.get(`https://deezer.eddypermana.com/album/${albumId}`);
         res.status(200).json({
-            message: "Data Top Genre",
+            message: "Album by ID",
             data: response.data.data
-        })
+        });
     } catch (error) {
         res.status(404).json({
             message: error.message
-        })
+        });
     }
 }
 
-const topTrack = async (req,res)  => {
+const albumTracks  = async (req, res) => {
     try {
-        const response = await axios.get('https://deezer.eddypermana.com/chart/0/tracks')
+        const albumId = req.params.albumId;
+        const response = await axios.get(`https://deezer.eddypermana.com/album/${albumId}/tracks`);
         res.status(200).json({
-            message: "Data Top Tracks",
+            message: "Album Tracks",
             data: response.data.data
-        })
+        });
     } catch (error) {
         res.status(404).json({
             message: error.message
-        })
+        });
     }
 }
 
-const topArtist = async (req,res)  => {
+// Artist
+const topArtists = async (req,res)  => {
     try {
-        const response = await axios.get('https://deezer.eddypermana.com/chart/0/artists')
+        const response = await axios.get('https://deezer.eddypermana.com/chart/0/artists');
         res.status(200).json({
-            message: "Data Top Artist",
+            message: "Top Artists",
             data: response.data.data
-        })
+        });
     } catch (error) {
         res.status(404).json({
             message: error.message
-        })
+        });
     }
 }
 
-const topPlaylist = async (req,res)  => {
+const artistById = async (req,res)  => {
     try {
-        const response = await axios.get('https://deezer.eddypermana.com/radio')
+        const artistId = req.params.artistId;
+        const response = await axios.get(`https://deezer.eddypermana.com/artist/${artistId}`);
         res.status(200).json({
-            message: "Data Top Playlist",
+            message: "Artist by ID",
             data: response.data.data
-        })
+        });
     } catch (error) {
         res.status(404).json({
             message: error.message
-        })
+        });
+    }
+}
+
+const artistTopTracks = async (req,res)  => {
+    try {
+        const artistId = req.params.artistId;
+        const response = await axios.get(`https://deezer.eddypermana.com/artist/${artistId}/top`);
+        res.status(200).json({
+            message: "Artist Top Tracks",
+            data: response.data.data
+        });
+    } catch (error) {
+        res.status(404).json({
+            message: error.message
+        });
+    }
+}
+
+const artistAlbums = async (req,res)  => {
+    try {
+        const artistId = req.params.artistId;
+        const response = await axios.get(`https://deezer.eddypermana.com/artist/${artistId}/albums`);
+        res.status(200).json({
+            message: "Artist Albums",
+            data: response.data.data
+        });
+    } catch (error) {
+        res.status(404).json({
+            message: error.message
+        });
+    }
+}
+
+const artistRelated = async (req,res)  => {
+    try {
+        const artistId = req.params.artistId;
+        const response = await axios.get(`https://deezer.eddypermana.com/artist/${artistId}/related`);
+        res.status(200).json({
+            message: "Artist Related",
+            data: response.data.data
+        });
+    } catch (error) {
+        res.status(404).json({
+            message: error.message
+        });
+    }
+}
+
+// Playlist
+const allPlaylists = async (req,res)  => {
+    try {
+        const response = await axios.get('https://deezer.eddypermana.com/radio');
+        res.status(200).json({
+            message: "All Playlists",
+            data: response.data.data
+        });
+    } catch (error) {
+        res.status(404).json({
+            message: error.message
+        });
+    }
+}
+
+const playlistById = async (req,res)  => {
+    try {
+        const playlistId = req.params.playlistId;
+        const response = await axios.get(`https://deezer.eddypermana.com/radio/${playlistId}`);
+        res.status(200).json({
+            message: "Playlist by ID",
+            data: response.data.data
+        });
+    } catch (error) {
+        res.status(404).json({
+            message: error.message
+        });
+    }
+}
+
+const playlistTracks = async (req,res)  => {
+    try {
+        const playlistId = req.params.playlistId;
+        const response = await axios.get(`https://deezer.eddypermana.com/radio/${playlistId}/tracks`);
+        res.status(200).json({
+            message: "Playlist Tracks",
+            data: response.data.data
+        });
+    } catch (error) {
+        res.status(404).json({
+            message: error.message
+        });
+    }
+}
+
+// Search
+const searchTracks = async (req,res)  => {
+    try {
+        const keyword = req.query.q;
+        const response = await axios.get(`https://deezer.eddypermana.com/search/track?q=${keyword}`);
+        res.status(200).json({
+            message: "Tracks Search Result",
+            data: response.data.data
+        });
+    } catch (error) {
+        res.status(404).json({
+            message: error.message
+        });
+    }
+}
+
+const searchAlbums = async (req,res)  => {
+    try {
+        const keyword = req.query.q;
+        const response = await axios.get(`https://deezer.eddypermana.com/search/album?q=${keyword}`);
+        res.status(200).json({
+            message: "Albums Search Result",
+            data: response.data.data
+        });
+    } catch (error) {
+        res.status(404).json({
+            message: error.message
+        });
+    }
+}
+
+const searchArtists = async (req,res)  => {
+    try {
+        const keyword = req.query.q;
+        const response = await axios.get(`https://deezer.eddypermana.com/search/artist?q=${keyword}`);
+        res.status(200).json({
+            message: "Artists Search Result",
+            data: response.data.data
+        });
+    } catch (error) {
+        res.status(404).json({
+            message: error.message
+        });
     }
 }
 
@@ -126,13 +310,26 @@ const topPlaylist = async (req,res)  => {
 //     }
 // }
 module.exports = {
-    topAlbum,
-    topGenre,
-    topArtist,
-    topTrack,
-    topPlaylist
-    // GetAllUser,
-    // addNewUser,
-    // deleteUser
-    // search
+    topTracks,
+
+    allGenres,
+    genreArtists,
+    
+    topAlbums,
+    albumById,
+    albumTracks,
+    
+    topArtists,
+    artistById,
+    artistTopTracks,
+    artistAlbums,
+    artistRelated,
+    
+    allPlaylists,
+    playlistById,
+    playlistTracks,
+    
+    searchTracks,
+    searchAlbums,
+    searchArtists
 }
